c47e6cfd14d6db41d95ad8bb9a0e4aa9bb154dc0
blindtolies <briankstephenson83@gmail.com>
2025-03-06 06:36:12 -0500

    Update and rename sample_config.py to config.py

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -1,18 +1,10 @@
-if not __name__.endswith("sample_config"):
-    import sys
-    print("The README is there to be read. Extend this sample config to a config file, don't just rename and change "
-          "values here. Doing that WILL backfire on you.\nBot quitting.", file=sys.stderr)
-    quit(1)
-
-
-# Create a new config.py file in same dir and import, then extend this class.
 class Config(object):
     LOGGER = True
 
     # REQUIRED
-    API_KEY = "YOUR KEY HERE"
-    OWNER_ID = "YOUR ID HERE"  # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "YOUR USERNAME HERE"
+    API_KEY = "YOUR ACTUAL API KEY"
+    OWNER_ID = "YOUR ACTUAL OWNER ID"  # If you don't know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "YOUR ACTUAL USERNAME"
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
@@ -26,7 +18,7 @@ class Config(object):
     SUDO_USERS = []  # List of id's (not usernames) for users which have sudo access to the bot.
     SUPPORT_USERS = []  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
     WHITELIST_USERS = []  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
-    DONATION_LINK = None  # EG, paypal
+    DONATION_LINK = None  # EG, PayPal
     CERT_PATH = None
     PORT = 5000
     DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
@@ -38,7 +30,6 @@ class Config(object):
 
 class Production(Config):
     LOGGER = False
-
 
 class Development(Config):
     LOGGER = True
