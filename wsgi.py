c70085d154599bfaadd269353af7168f8e170e05
blindtolies <briankstephenson83@gmail.com>
2025-03-06 06:46:21 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,15 +2,21 @@ import sys
 import os
 
 # Add your project directory to the sys.path
-project_home = '/home/your_username/SHALL-Bot'
+project_home = '/home/yourusername/SHALL-Bot'
 if project_home not in sys.path:
     sys.path = [project_home] + sys.path
+
+# Activate your virtual environment
+activate_this = os.path.expanduser('~/.virtualenvs/yourenv/bin/activate_this.py')
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
 
 # Set the PYTHONPATH to include your project directory
 os.environ['PYTHONPATH'] = project_home
 
-# Start your bot
+# Import and start your bot
 import tg_bot
 
 if __name__ == "__main__":
-    tg_bot.main()
+    from tg_bot import updater
+    updater.start_polling()
